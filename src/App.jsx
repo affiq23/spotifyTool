65c73a263d34cb53fcfc43e8b836648d5b671827
affiq23 <affiqem@gmail.com>
2024-01-02 23:41:29 -0600

    called all data from API

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 import Dropdown from './Dropdown';
+import Listbox from './Listbox';
 import axios from 'axios';
 
+function Credentials() {
+  return {
+    ClientId: 'c6973fef14f245c78aad2cdcf66983d9', // taken from Spotify for Developers website
+    ClientSecret: 'fd0455c45fd849bda7f8f389b0c984a3'
+  };
+}
+
 function App() {
+
+  const [token, setToken] = useState("");
+  const [genres, setGenres] = useState({selectedGenre: "", listGenresAPI: []});
+  const [playlist, setPlaylist] = useState({selectedPlaylist: "", listPlaylistAPI: []});
+  const [tracks, setTracks] = useState({selectedTrack: "", listTrackAPI: []});
+  const [trackDetail, setTrackDetail] = useState(null);
+  const spotify = Credentials();
 
   const data = [
     {value: 1, name: "A"},
@@ -12,17 +27,7 @@ function App() {
     {value: 3, name: "C"}
 ]
 
-  const [token, setToken] = useState("")
-
-
-function Credentials() {
-  return {
-    ClientId: 'c6973fef14f245c78aad2cdcf66983d9',
-    ClientSecret: 'fd0455c45fd849bda7f8f389b0c984a3'
-  };
-}
-
-const spotify = Credentials();
+  
 
   useEffect(() => {
     axios("https://accounts.spotify.com/api/token", {
@@ -36,18 +41,76 @@ const spotify = Credentials();
       .then(tokenRes => {
         console.log("tokenResponse.data.access_token");
         setToken(tokenRes.data.access_token);
-      })
-  }, [])
 
+        axios("https://api.spotify.com/v1/browse/categories?locale=sv_US", {
+          method: "GET",
+          headers: { "Authorization" : "Bearer " + tokenRes.data.access_token}
+        })
+          .then(genreRes => {
+            setGenres({
+              selectedGenre: genres.selectedGenre,
+              listGenresAPI: genreRes.data.categories.items
+            })
+          })
+      })
+  }, [genres.selectedGenre, spotify.ClientId, spotify.ClientSecret]);
+
+  const genreChanged = val => {
+    setGenres({
+      selectedGenre : val,
+      listGenresAPI: genres.listGenresAPI
+    })
+    axios(`https://api.spotify.com/v1/browse/categories/${val}/playlists?limit=10`, {
+      method: "GET",
+      headers: { "Authorization" : "Bearer " + token}
+  }) 
+    .then(playlistRes => {
+      setPlaylist({
+        selectedPlaylist: playlist.selectedPlaylist,
+        listPlaylistAPI: playlistRes.data.playlists.items
+      });
+    })
+    console.log(val);
+  };
+
+    const playlistChanged = val => {
+      console.log(val);
+      setPlaylist({
+        selectedPlaylist: val,
+        listPlaylistAPI: playlist.listPlaylistAPI
+      })
+    }
+
+    const buttonClicked = e => {
+      e.preventDefault();
+
+      axios(`https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks?limit=10`, {
+        method: "GET",
+        headers: { "Authorization" : "Bearer " + token}
+      })
+        .then(trackRes => {
+          setTracks({
+            selectedTrack: tracks.selectedTrack,
+            listTrackAPI: trackRes.data.items
+          })
+        })
+    }
+
+    const listboxClicked = val => {
+      const currentTracks = [...tracks.listTrackAPI];
+      const trackInfo = currentTracks.filter(t => t.track.id === val);
+      setTrackDetail(trackInfo[0].track);
+    }
 
   return (
-      <form onSubmit={() => {}}>
+      <form onSubmit={buttonClicked}>
           <div className="container">
-          <Dropdown options={data} /> 
-          <Dropdown options={data} />
+          <Dropdown options={genres.listGenresAPI} selectedVal={genres.selectedGenre} changed={genreChanged} /> 
+          <Dropdown options={playlist.listPlaylistAPI} selectedVal={playlist.selectedPlaylist} changed={playlistChanged} />
           <button type="submit">
             Search
           </button>
+          <Listbox items={tracks.listTrackAPI} clicked={listboxClicked} />
           </div>
       </form>
   )
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Dropdown = props => {
 
     const [selectedVal, setSelectedVal] = useState("");
+
+    const dropdownChanged = e => {
+        props.changed(e.target.value);
+    }
     
     return ( 
         <div>
-            <select value={selectedVal} onChange={e => setSelectedVal(e.target.value)}>
-                {props.options.map((item, idx) => <option key={idx} value={item.value}>{item.name}</option>)}
+            <select value={props.selectedVal} onChange={dropdownChanged}>
+                {props.options.map((item, idx) => <option key={idx} value={item.id}>{item.name}</option>)}
             </select>
             <p>{selectedVal}</p>
         </div>
